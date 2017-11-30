8f745fe747f8de23714c1e44e15afa6a2145b44e
dayamll <dayamllerenacastro@gmail.com>
2017-11-30 12:12:59 -0500

    Añadiendo README y google charts

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   var sede = document.getElementById('sede');
   var generacion = document.getElementById('generation');
-  
+
   var city = '';
   var generation = '';
   students = {};
-  
+
   generacion.addEventListener('change', totalStudents);
-   
+
   function totalStudents(event) {
     city = sede.value;
     generation = generacion.value;
@@ -22,30 +22,37 @@ window.addEventListener('load', function() {
   }
 });
 
-// La cantidad de estudiantes que superan la meta de puntos en promedio
-//  de todos los sprints cursados. La meta de puntos es 70% del total de puntos en HSE y en tech.
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {
+  'packages': ['corechart']
+});
 
-var techS = document.getElementById('techS');
-var techSkills = document.createElement('div');
-var titleTS = document.createElement('h1');
-titleTS.innerText = 'TECH SKILLS';
-var comboTS = document.createElement('select');
-var OP1 = document.createElement('option');
-OP1.innerText = 'Sprint 1';
-var OP2 = document.createElement('option');
-OP2.innerText = 'Sprint 1';
-var OP3 = document.createElement('option');
-OP3.innerText = 'Sprint 3';
-var OP4 = document.createElement('option');
-OP4.innerText = 'Sprint 4';
-var metTarget = document.createElement('div');
-var titleMT = document.createElement('h1');
-titleMT.innerText = 'numberofpercent';
-var textMT = document.createElement('span');
-textMT.innerText = '# STUDENTS THAT MEET THE TARGET';
-var totalTS = document.createElement('div');
-var titleTS2 = document.createElement('h1');
-titleTS2.innerText = 'percentoftotal';
-var textTS = document.createElement('span');
-textTS.innerText = '% Of TOTAL';
-// agregando clases
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['activos', 5],
+    ['inactivos', 5.8],
+  ]);
+
+  // Set chart options
+  var options = {
+    'title': 'Estado de las alumnas',
+    'width': 450,
+    'height': 300
+  };
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
+
